decc694c2d2abede28b28bc9ffe32d8ae32a5e9c
gongyisheng <yishenggong9437@gmail.com>
2026-04-10 08:45:03 -0700

    chore: test on using deterministic algo

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -34,6 +34,9 @@ class Trainer:
         torch.backends.cuda.matmul.allow_tf32 = True
         torch.backends.cudnn.allow_tf32 = True
 
+        # Force deterministic CUDA algorithms
+        os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+        torch.use_deterministic_algorithms(True)
 
         # Seed for reproducibility
         self._seed(42)
