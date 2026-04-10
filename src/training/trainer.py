3e860938e7440973e74948f4313d327fb5ce1fc1
gongyisheng <yishenggong9437@gmail.com>
2026-04-10 08:12:22 -0700

    chore: remove torch.backends.cudnn.benchmark=true

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -32,7 +32,6 @@ class Trainer:
         torch.set_float32_matmul_precision('high')
         torch.backends.cuda.matmul.allow_tf32 = True
         torch.backends.cudnn.allow_tf32 = True
-        torch.backends.cudnn.benchmark = True
 
 
         # Seed for reproducibility
